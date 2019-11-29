6975d003e556ca84ab18f1056c9ce01ee310cf43
Prakhar Baveja <prakhar.baveja@gmail.com>
2019-11-29 17:06:42 +0530

    promisified loop for downloads

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var progress = require('request-progress');
 const fs = require('fs');
 const dir = './OUTPUT/';
 
-async function processItems(feed, podcastFolder){
-	for (const item of feed.items) {
-		console.log("-------------------------------------")
-		console.log("Episode data: ", item.title + ':' + item.enclosure.url) // title of episodes + play url
+function processItems(item, podcastFolder) {
+	return new Promise(async (resolve, reject) => {
+		console.log('\x1b[42m%s\x1b[0m',"EPISODE")
+		console.log("Title: " + item.title + ', Play URL:' + item.enclosure.url) // title of episodes + play url
 
 		// Make episode folder
 		const episodeFolder = podcastFolder + '/' + item.title; //'Episode '+ index +' - '+
@@ -18,28 +18,35 @@ async function processItems(feed, podcastFolder){
 		if (!fs.existsSync(episodeFolder)){
 			await fs.mkdirSync(episodeFolder);
 		}
+		
+		console.log('\x1b[34m%s\x1b[0m',"Starting episode download...")
 
 	    const response = await progress(request(item.enclosure.url))
 	    .on('progress', function (state) {
-			console.log("Percentage done: ", state.percent*100 + '%')
+			console.log("Progress :: ", (state.percent*100).toFixed(2) + '%')
 		})
 		.on('error', function (err) {
-			console.log('\x1b[31m%s\x1b[0m', "ERROR");
 			console.log(err);
+			reject("ERROR");
 		})
 		.on('end', function () {
-			console.log('\x1b[32m%s\x1b[0m', "File download completed!");
+			resolve("File download completed!");
 		})
 		.pipe(fs.createWriteStream(episodeFolder + '/' + item.title+'.mp3'));
-	}
+	});
 };
 
 (async () => {
-	for (var i = 0; i < 1; i++) { // data.podcasts.length; i++) {
+	for (var i = 0; i < data.podcasts.length; i++) {
 		console.log("Data Title: ", data.podcasts[i].title); // title of podcast from data
 		console.log("=====================================")
 		let feed = await parser.parseURL(data.podcasts[i].rss);
 		
+		// Make OUTPUT folder if it doesn't exist
+		if (!fs.existsSync(dir)){
+			await fs.mkdirSync(dir);
+		}
+
 		// Make directory with podcast title
 		const podcastFolder = dir + data.podcasts[i].title;
 		if (!fs.existsSync(podcastFolder)){
@@ -48,35 +55,15 @@ async function processItems(feed, podcastFolder){
 
 		console.log("RSS Title: ",feed.title); // title from rss
 		console.log("=====================================")
-
-		processItems(feed, podcastFolder);
+		for (const item of feed.items) {
+			const res = await processItems(item, podcastFolder);
+			if (res === "ERROR") {
+				console.log('\x1b[31m%s\x1b[0m', res);
+			} else {
+				console.log('\x1b[32m%s\x1b[0m', res);
+				console.log("=====================================")
+			}
+		}
 	}
 
 })();
-
-// async downloadSaveEpisode(item) {
-// 	console.log("-------------------------------------")
-// 	console.log("Episode data: ", item.title + ':' + item.enclosure.url) // title of episodes + play url
-// 	console.log("-------------------------------------")
-	
-// 	// Make episode folder
-// 	const episodeFolder = podcastFolder + '/' + item.title; //'Episode '+ index +' - '+
-// 	// console.log("FOLDER",episodeFolder);
-// 	if (!fs.existsSync(episodeFolder)){
-// 		await fs.mkdirSync(episodeFolder);
-// 	}
-
-// 	// Download mp3 and write into respective episode folder
-// 	await progress(await request(item.enclosure.url), {})
-// 	.on('progress', function (state) {
-// 		console.log("Percentage done: ", state.percent*100 + '%')
-// 	})
-// 	.on('error', function (err) {
-// 		console.log('\x1b[31m%s\x1b[0m', "ERROR");
-// 		console.log(err);
-// 	})
-// 	.on('end', function () {
-// 		console.log('\x1b[32m%s\x1b[0m', "File download completed!");
-// 	})
-// 	.pipe(fs.createWriteStream(episodeFolder + '/' + item.title+'.mp3'));
-// }
